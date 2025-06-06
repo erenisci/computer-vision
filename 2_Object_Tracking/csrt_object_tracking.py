a53978cfa38ab1e4002c01e5d5b3b6dc95d31f1a
Eren İşci <isci.eren@gmail.com>
2025-06-07 00:21:16 +0300

    object_tracking

---
+++ 2_Object_Tracking/csrt_object_tracking.py
@@ -0,0 +1,38 @@
+# CSRT (Channel and Spatial Reliability Tracking)
+import cv2
+
+tracker = cv2.TrackerCSRT_create()
+video = cv2.VideoCapture('Videos/race.mp4')
+
+# FPS of the video
+# fps = video.get(cv2.CAP_PROP_FPS) # 30
+# delay = int(1000 / fps)
+
+# Static delay
+delay = 1
+
+ok, frame = video.read()
+bbox = cv2.selectROI("Tracking", frame, False)
+
+ok = tracker.init(frame, bbox)
+
+while True:
+    ok, frame = video.read()
+
+    if not ok:
+        break
+
+    ok, bbox = tracker.update(frame)
+
+    if ok:
+        (x, y, w, h) = [int(v) for v in bbox]
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2, 1)
+        cv2.putText(frame, 'Object', (x, y + h + 30),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 2)
+    else:
+        cv2.putText(frame, 'Error', (110, 55),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
+    cv2.imshow('Tracking', frame)
+    if cv2.waitKey(delay) & 0XFF == 27:  # ESC
+        break
